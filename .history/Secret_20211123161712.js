e5f07486c96ef6ecccae7c3c314a7c7a2267e6a4
90808047+atsuya23@users.noreply.github.com <90808047+atsuya23@users.noreply.github.com>
2021-11-23 16:22:12 +0900

    "ok?"

---
+++ .history/Secret_20211123161712.js
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+export const RESTAPI_URL = "https://training-rest.herokuapp.com/"
+=======
+export const RESTAPI_URL = "https://training-rest.herokuapp.com/"
+>>>>>>> f15b415a1a3a7876983e425bf4ebea3fcf1b4b66
